96e108574d56296a2c32af16bf596bc91ee43488
epsilon99999 <2303282@students.wits.ac.za>
2022-09-21 15:48:50 +0200

    Working on Adding to on screen inventory

--- Assets/Zoe's Stuff/Scripts/AddToInventory.cs
+++ Assets/Zoe's Stuff/Scripts/AddToInventory.cs
@@ -21,42 +21,42 @@ public class AddToInventory : MonoBehaviour
             bookScript.slotOne.GetComponent<Image>().enabled = true;
             bookScript.slotOne.GetComponent<Image>().sprite = this.gameObject.GetComponent<Image>().sprite;
             bookScript.oneEmpty = false;
-            this.gameObject.SetActive(false);
+            Destroy(gameObject);
         }
         else if (slotManager.GetComponent<InventoryManager>().twoEmpty)
         {
             bookScript.slotTwo.GetComponent<Image>().enabled = true;
             bookScript.slotTwo.GetComponent<Image>().sprite = this.gameObject.GetComponent<Image>().sprite;
             bookScript.twoEmpty = false;
-            this.gameObject.SetActive(false);
+            Destroy(gameObject);
         }
         else if (slotManager.GetComponent<InventoryManager>().threeEmpty)
         {
             bookScript.slotThree.GetComponent<Image>().enabled = true;
             bookScript.slotThree.GetComponent<Image>().sprite = this.gameObject.GetComponent<Image>().sprite;
             bookScript.threeEmpty = false;
-            this.gameObject.SetActive(false);
+            Destroy(gameObject);
         }
         else if (slotManager.GetComponent<InventoryManager>().fourEmpty)
         {
             bookScript.slotFour.GetComponent<Image>().enabled = true;
             bookScript.slotFour.GetComponent<Image>().sprite = this.gameObject.GetComponent<Image>().sprite;
             bookScript.fourEmpty = false;
-            this.gameObject.SetActive(false);
+            Destroy(gameObject);
         }
         else if (slotManager.GetComponent<InventoryManager>().fiveEmpty)
         {
             bookScript.slotFive.GetComponent<Image>().enabled = true;
             bookScript.slotFive.GetComponent<Image>().sprite = this.gameObject.GetComponent<Image>().sprite;
             bookScript.fiveEmpty = false;
-            this.gameObject.SetActive(false);
+            Destroy(gameObject);
         }
         else if (slotManager.GetComponent<InventoryManager>().sixEmpty)
         {
             bookScript.slotSix.GetComponent<Image>().enabled = true;
             bookScript.slotSix.GetComponent<Image>().sprite = this.gameObject.GetComponent<Image>().sprite;
             bookScript.sixEmpty = false;
-            this.gameObject.SetActive(false);
+            Destroy(gameObject);
         }
         else
         {
