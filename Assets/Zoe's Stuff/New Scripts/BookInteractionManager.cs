463b14461909fdd92ef6cbc5be40d890d95d2375
michaela-dupreez <2321949@students.wits.ac.za>
2022-10-25 20:02:25 +0200

    Revert "Decor Stuff"
    This reverts commit c999df119a9ce9223e62cbfe273d1dacb660a0af, reversing
    changes made to b36b952bcc4c5402433e30ffb8dcda29500b365e.

--- Assets/Zoe's Stuff/New Scripts/BookInteractionManager.cs
+++ Assets/Zoe's Stuff/New Scripts/BookInteractionManager.cs
@@ -9,9 +9,6 @@ public class BookInteractionManager : MonoBehaviour, IDragHandler, IEndDragHandl
     private CanvasGroup trialCanvasGroup;
 
     [SerializeField]
-    private Canvas dragNDrop;
-
-    [SerializeField]
     private GameObject slot1;
     [SerializeField]
     private GameObject slot2;
@@ -35,8 +32,6 @@ public class BookInteractionManager : MonoBehaviour, IDragHandler, IEndDragHandl
     {
         transform.position = Input.mousePosition;
         trialCanvasGroup.blocksRaycasts = false;
-        transform.parent = dragNDrop.transform;
-        this.gameObject.GetComponent<Transform>().localScale = new Vector3(0.7f, 0.7f, 0.7f);
     }
 
     public void OnEndDrag(PointerEventData eventData)
