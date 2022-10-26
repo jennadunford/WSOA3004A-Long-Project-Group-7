3ea349ca3b5fe27dcd7ef261f4b54719cd915aa1
epsilon99999 <2303282@students.wits.ac.za>
2022-10-26 14:46:35 +0200

    Books Working

--- Assets/Zoe's Stuff/New Scripts/BookInteractionManager.cs
+++ Assets/Zoe's Stuff/New Scripts/BookInteractionManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class BookInteractionManager : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerClickHandler
+public class BookInteractionManager : MonoBehaviour, IDragHandler, IEndDragHandler
 {
     public bool inInvent = false;
     private CanvasGroup trialCanvasGroup;
@@ -11,29 +11,12 @@ public class BookInteractionManager : MonoBehaviour, IDragHandler, IEndDragHandl
     [SerializeField]
     private Canvas dragCanvas;
 
-    [SerializeField]
-    private GameObject slot1;
-    [SerializeField]
-    private GameObject slot2;
-    [SerializeField]
-    private GameObject slot3;
-    [SerializeField]
-    private GameObject slot4;
-
-    private GameObject[] slotsArray = new GameObject[4];
-
     private void Start()
     {
         trialCanvasGroup = gameObject.GetComponent<CanvasGroup>();
-
-        slotsArray[0] = slot1;
-        slotsArray[1] = slot2;
-        slotsArray[2] = slot3;
-        slotsArray[3] = slot4;
     }
     public void OnDrag(PointerEventData eventData)
     {
-        //transform.position = Input.mousePosition;
         gameObject.GetComponent<RectTransform>().position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
         trialCanvasGroup.blocksRaycasts = false;
         transform.parent = dragCanvas.transform;
@@ -44,20 +27,5 @@ public class BookInteractionManager : MonoBehaviour, IDragHandler, IEndDragHandl
     {
         transform.localPosition = Vector3.zero;
         trialCanvasGroup.blocksRaycasts = true;
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (!inInvent)
-        {
-            for (int i = 0; slotsArray[i].transform.childCount == 0 && i < 4; i++)
-            {
-                this.gameObject.transform.parent = slotsArray[i].transform;
-                this.gameObject.GetComponent<Transform>().position = slotsArray[i].GetComponent<Transform>().position;
-                inInvent = true;
-                
-
-            }
-        }
     }
 }
---
+++ Assets/Zoe's Stuff/New Scripts/DropOnTrolley.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropOnTrolley : MonoBehaviour, IDropHandler
+{
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (gameObject.transform.childCount < 4)
+        {
+            eventData.pointerDrag.GetComponent<CanvasGroup>().blocksRaycasts = true;
+            eventData.pointerDrag.transform.parent = this.gameObject.transform;
+            eventData.pointerDrag.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
