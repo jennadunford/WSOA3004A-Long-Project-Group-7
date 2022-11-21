7d0f758d8f9ab6c4e5dc8af8070eebaae5d325b4
epsilon99999 <2303282@students.wits.ac.za>
2022-11-21 16:38:37 +0200

    UI Tutorialization

---
+++ Assets/Michaela's Stuff/Tutorial/IntroUI.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject text1;
+    [SerializeField]
+    private GameObject text2;
+    [SerializeField]
+    private GameObject text3;
+
+    [SerializeField]
+    private GameObject glowClock;
+    [SerializeField]
+    private GameObject glowBookmark;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Tutorial());
+    }
+
+    
+    private IEnumerator Tutorial()
+    {
+        text1.SetActive(true);
+
+        yield return new WaitForSeconds(2);
+
+        text1.SetActive(false);
+        text2.SetActive(true);
+        glowClock.SetActive(true);
+
+        yield return new WaitForSeconds(4);
+
+        text2.SetActive(false);
+        glowClock.SetActive(false);
+        text3.SetActive(true);
+        glowBookmark.SetActive(true);
+
+        yield return new WaitForSeconds(4);
+
+        text3.SetActive(false);
+        glowBookmark.SetActive(false);
+    }
+}
