00cd5c33e6d1522035967a410fb4f05f0c4ce832
michaela-dupreez <2321949@students.wits.ac.za>
2022-11-22 12:14:59 +0200

    Requests

--- Assets/Michaela's Stuff/Tutorial/IntroUI.cs
+++ Assets/Michaela's Stuff/Tutorial/IntroUI.cs
@@ -28,20 +28,20 @@ public class IntroUI : MonoBehaviour
     {
         text1.SetActive(true);
 
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(4);
 
         text1.SetActive(false);
         text2.SetActive(true);
         glowClock.SetActive(true);
 
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(10);
 
         text2.SetActive(false);
         glowClock.SetActive(false);
         text3.SetActive(true);
         glowBookmark.SetActive(true);
 
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(10);
 
         text3.SetActive(false);
         glowBookmark.SetActive(false);
